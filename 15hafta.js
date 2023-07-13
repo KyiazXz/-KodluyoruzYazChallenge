c193294aceed145ad30e740a704c24a0739f0846
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-07-14 00:13:48 +0300

    easy part done

---
+++ 15hafta.js
@@ -0,0 +1,10 @@
+// easy
+
+//  Kullanıcının doğum tarihini alarak kaç yaşında olduğunu bulan bir algoritma yazmanızı istiyorum.
+
+// function dogumTarih(year){
+//     let date =  new Date().getFullYear();
+//     let result = date - year
+//     console.log(result)
+// }
+// dogumTarih(2001)
